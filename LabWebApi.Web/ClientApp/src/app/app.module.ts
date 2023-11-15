3fc79a245dd4940255300f63e9da22b0feda4e3c
olenashevchuk <olenashevchuk0@gmail.com>
2023-11-15 11:25:58 +0200

    Set up modules, add components #done

--- LabWebApi.Web/ClientApp/src/app/app.module.ts
+++ LabWebApi.Web/ClientApp/src/app/app.module.ts
@@ -1,18 +1,44 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { HomeComponent } from './pages/home-components/home/home.component';
+import { HttpClientModule } from '@angular/common/http';
+import { JwtModule } from '@auth0/angular-jwt';
+import { LoginComponent } from './pages/auth-components/login/login.component';
+import { MatModule } from './core/modules/mat.module';
+import { NgModule } from '@angular/core';
+import { RegistrationComponent } from './pages/auth-components/registration/registration.component';
+import { RxReactiveFormsModule } from '@rxweb/reactive-form-validators';
+
+export function tokenGetter() { return localStorage.getItem('token'); }
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegistrationComponent,
+    HomeComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RxReactiveFormsModule,
+    HttpClientModule,
+    MatModule,
+    FlexLayoutModule,
+    JwtModule.forRoot({ config: { tokenGetter } })
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ LabWebApi.Web/ClientApp/src/app/core/modules/mat.module.ts
@@ -0,0 +1,39 @@
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatOptionModule } from '@angular/material/core';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatTableModule } from '@angular/material/table';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { NgModule } from '@angular/core';
+
+@NgModule({
+  imports: [
+    MatToolbarModule,
+    MatInputModule,
+    MatCardModule,
+    MatMenuModule,
+    MatIconModule,
+    MatButtonModule,
+    MatTableModule,
+    MatSlideToggleModule,
+    MatSelectModule,
+    MatOptionModule,
+  ],
+  exports: [
+    MatToolbarModule,
+    MatInputModule,
+    MatCardModule,
+    MatMenuModule,
+    MatIconModule,
+    MatButtonModule,
+    MatTableModule,
+    MatSlideToggleModule,
+    MatSelectModule,
+    MatOptionModule,
+  ],
+})
+export class MatModule {}

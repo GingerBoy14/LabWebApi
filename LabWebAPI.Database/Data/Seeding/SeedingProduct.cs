bff7df412d048c547f531e4e12ce45298120f88c
olenashevchuk <olenashevchuk0@gmail.com>
2023-12-19 00:54:50 +0200

    update intial product creation method

--- LabWebAPI.Database/Data/Seeding/SeedingProduct.cs
+++ LabWebAPI.Database/Data/Seeding/SeedingProduct.cs
@@ -5,6 +5,7 @@ using LabWebAPI.Contracts.Roles;
 using Microsoft.AspNetCore.Identity;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -21,16 +22,28 @@ namespace LabWebAPI.Database.Data.Seeding
             var admins = await userManager.GetUsersInRoleAsync(AuthorizationRoles.Admin.ToString());
             if (admins != null && admins.Count > 0) {
                 var admin = admins.First(); // Вибрати першого адміністратора
-                var testProduct = new Product
+
+                // Перевірка, чи існують продукти вже в системі
+                var productCount = await productRepository.Query().CountAsync();
+
+                if (productCount <= 0)
                 {
-                    Name = "test",
-                    Description = "test product",
-                    Price = 500,
-                    PublicationDate = DateTime.Now,
-                    UserWhoCreatedId = admin.Id
-                };
-                await productRepository.AddAsync(testProduct);
-                await productRepository.SaveChangesAsync();
+                    var testProduct = new Product
+                    {
+                        Name = "test",
+                        Description = "test product",
+                        Price = 500,
+                        PublicationDate = DateTime.Now,
+                        UserWhoCreatedId = admin.Id
+                    };
+
+                    await productRepository.AddAsync(testProduct);
+                    await productRepository.SaveChangesAsync();
+                }
+                else
+                {
+                    Console.WriteLine("Test product already exists in the system.");
+                }
             }
             else
             {

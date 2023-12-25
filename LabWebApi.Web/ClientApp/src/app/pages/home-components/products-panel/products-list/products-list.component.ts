de8315ff73c5344a031b7df8dee3df971c0e1b38
olenashevchuk <olenashevchuk0@gmail.com>
2023-12-25 22:13:25 +0200

    implemented seach by name in product list

--- LabWebApi.Web/ClientApp/src/app/pages/home-components/products-panel/products-list/products-list.component.ts
+++ LabWebApi.Web/ClientApp/src/app/pages/home-components/products-panel/products-list/products-list.component.ts
@@ -17,41 +17,68 @@ import { ProductService } from './../../../../core/services/Product.service';
 })
 export class ProductsListComponent implements OnInit {
   products: ProductInfo[];
+  filteredProduct: ProductInfo[];
+  searchQuery: string;
   constructor(
     private productService: ProductService,
-    private alertService: AlertService,   
+    private alertService: AlertService,
     private dialog: MatDialog,
-    private authService: AuthenticationService,
+    private authService: AuthenticationService
   ) {}
   async ngOnInit() {
-    this.productService
-      .getProducts()
-      .subscribe((data: ProductInfo[]) => (this.products = data));
+    this.productService.getProducts().subscribe((data: ProductInfo[]) => {
+      this.products = data;
+      this.filteredProduct = data;
+    });
   }
 
   isCurrentUserAdmin(): boolean {
     return this.authService.currentUser.role == AuthorizationRoles.Admin;
   }
-  isPossibleToEdit(product:ProductInfo):boolean{
-    return this.isCurrentUserAdmin() || product.userWhoCreated.id=== this.authService.currentUser.id
+  isPossibleToEdit(product: ProductInfo): boolean {
+    return (
+      this.isCurrentUserAdmin() ||
+      product.userWhoCreated.id === this.authService.currentUser.id
+    );
+  }
+
+  onSearchByName() {
+    console.log(this.searchQuery);
+    if (!this.searchQuery) {
+      this.filteredProduct = this.products;
+    } else {
+      this.filteredProduct = this.products.filter((product: ProductInfo) =>
+        product.name
+          .toLocaleLowerCase()
+          .startsWith(this.searchQuery.toLocaleLowerCase())
+      );
+    }
   }
 
   async editProduct(product: ProductInfo) {
-      console.log(product)
+    console.log(product);
     const dialogRef = this.dialog.open(EditProductDialogComponent, {
       data: { ...product },
     });
     dialogRef.afterClosed().subscribe((result: SimpleProductInfo) => {
       if (result) {
-        this.productService.editProduct({...result, id: product.id, publicationDate:product.publicationDate}).subscribe((data: any) => {
-          let modelIndex = this.products.findIndex((x: any) => x.id == data.id);
-          if (modelIndex !== -1) {
-            this.products.splice(modelIndex, 1, data);
-            this.products = [...this.products];
-          } else {
-            this.alertService.errorAlert('Update Product', 'Failed!');
-          }
-        });
+        this.productService
+          .editProduct({
+            ...result,
+            id: product.id,
+            publicationDate: product.publicationDate,
+          })
+          .subscribe((data: any) => {
+            let modelIndex = this.products.findIndex(
+              (x: any) => x.id == data.id
+            );
+            if (modelIndex !== -1) {
+              this.products.splice(modelIndex, 1, data);
+              this.products = [...this.products];
+            } else {
+              this.alertService.errorAlert('Update Product', 'Failed!');
+            }
+          });
       }
     });
   }
@@ -67,14 +94,15 @@ export class ProductsListComponent implements OnInit {
     }
   }
 
-  async createProduct(){
-      const dialogRef = this.dialog.open(CreateProductDialogComponent );
-      dialogRef.afterClosed().subscribe((result: SimpleProductInfo) => {
+  async createProduct() {
+    const dialogRef = this.dialog.open(CreateProductDialogComponent);
+    dialogRef.afterClosed().subscribe((result: SimpleProductInfo) => {
       if (result) {
-        
-        this.productService.createProduct({...result, publicationDate:new Date()}).subscribe((data: any) => {
-         this.products = [...this.products, data]
-        });
+        this.productService
+          .createProduct({ ...result, publicationDate: new Date() })
+          .subscribe((data: any) => {
+            this.products = [...this.products, data];
+          });
       }
     });
   }
